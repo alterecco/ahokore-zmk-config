730be293c2af2ebd2896e5ae454ab1926a439ec6
alterecco <alterecco@slowmail.it>
2021-08-30 19:20:57 +0200

    update ahokore keymap

--- config/boards/arm/ahokore/ahokore.h
+++ config/boards/arm/ahokore/ahokore.h
@@ -1,16 +1,5 @@
-#define XXX &none
-#define ___ &trans
-
+// LAYERS
 #define DEF 0
 #define NAV 1
 #define SYM 2
 #define NUM 3
-#define ADJ 4
-
-
-#define NEXT LC(PG_DN)
-#define PREV LC(PG_UP)
-#define SW_APP LG(TAB)
-#define WORD_BS LC(BSPC)
-#define WORD_L LC(LEFT)
-#define WORD_R LC(RIGHT)
